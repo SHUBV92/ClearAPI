a7e9238289c26a1a8367f7c8ead9f0e0ffafa5ff
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-02-27 15:57:19 +0000

    Called on API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
+const API_KEY = "9106bf97-aa4f-466f-9181-1bfab08c85e9"
 
 class App extends Component{
+
+  state = {
+    images: [],
+    API: API_KEY
+  }
+
+  componentDidMount(){
+    fetch('https://api.thecatapi.com/v1/images/search?limit=6&page=100&order=DESC')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({images: data})
+    })
+    .catch(console.log)
+  }
+
 render() {
- return (
+ console.log(this.state.images)
+  return this.state.images.length === 0 ?
+  'Loading...' :
+
+    <ul className="">
+      {this.state.images.map(imgObj => (
+        <li>
+          <img src={imgObj.url}className="cat-img" />
+        </li>
+      ))}
+    </ul>
   
-  )
   }
 }
 
